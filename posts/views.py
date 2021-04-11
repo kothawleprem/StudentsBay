3272116d061747b4f91ba5a2a8c8a2f8181e6ee5
kothawleprem <kothawleprem@gmail.com>
2021-04-11 18:46:33 +0530

    Most Complete

--- posts/views.py
+++ posts/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render,HttpResponse,redirect
 from django.contrib import messages
 from .models import Post,PostComment
-
+from django.core.mail import send_mail
+from django.conf import settings
+from django.core.mail import EmailMultiAlternatives
 # Create your views here.
 def postHome(request):
     allPosts = Post.objects.all().order_by('-sno')
+
     context = {
         'allPosts' : allPosts,
     }
@@ -12,6 +15,10 @@ def postHome(request):
 
 def post(request, slug):
     post = Post.objects.filter(slug=slug).first()
+    global postcontent
+    global posttitle
+    postcontent = post.content
+    posttitle = post.title
     post.save()
     comments = PostComment.objects.filter(post=post).order_by('-sno')
     context = {
@@ -34,7 +41,16 @@ def postComment(request):
 
 def emailme(request):
     if request.method == "POST":
-        content = request.POST.get("uemail")
+        uemail = request.POST.get("uemail")
         # content = request.POST.get("value")
-        print(content)
+        print(uemail)
+        print(postcontent)
+        print(posttitle)
+        subject, from_email, to = 'Your Post From StudentsBay has Arrived!!!', settings.EMAIL_HOST_USER,uemail
+        text_content = 'Chart Sent Successfully'
+        html_content = f"<center><h1>{posttitle}</h1></center>{postcontent}"
+        msg = EmailMultiAlternatives(subject, text_content, from_email, [to])
+        msg.attach_alternative(html_content, "text/html")
+        msg.send()
+
     return redirect(f'/posts')
